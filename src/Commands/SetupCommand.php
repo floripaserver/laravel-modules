529d53c1fb5d211be77f51e139e360dea538681b
xuanhoa88 <xuan.0211@gmail.com>
2017-01-06 09:30:40 +0700

    Update SetupCommand.php

--- src/Commands/SetupCommand.php
+++ src/Commands/SetupCommand.php
@@ -2,32 +2,23 @@
 
 namespace Llama\Modules\Commands;
 
-use Illuminate\Support\Str;
-use RuntimeException;
-use Llama\Modules\Module;
-use Llama\Modules\Repository;
-use Llama\Modules\Traits\ModuleCommandTrait;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputOption;
-use Illuminate\Console\Command as BaseCommand;
+use Illuminate\Console\Command;
 
-class SeedCommand extends BaseCommand
+class SetupCommand extends Command
 {
-    use ModuleCommandTrait;
-
     /**
      * The console command name.
      *
      * @var string
      */
-    protected $name = 'module:seed';
+    protected $name = 'module:setup';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Run database seeder from the specified module or from all modules.';
+    protected $description = 'Setting up modules folders for first use.';
 
     /**
      * Execute the console command.
@@ -36,153 +27,50 @@ class SeedCommand extends BaseCommand
      */
     public function fire()
     {
-        try {
-            if ($name = $this->argument('module')) {
-                $name = Str::studly($name);
-                $this->moduleSeed($this->getModuleByName($name));
-            } else {
-                $modules = $this->getModuleRepository()->getOrdered();
-                array_walk($modules, [$this, 'moduleSeed']);
-                $this->info('All modules seeded.');
-            }
-        } catch (\Exception $e) {
-            $this->error($e->getMessage());
-        }
+        $this->generateModulesFolder();
+
+        $this->generateAssetsFolder();
     }
 
     /**
-     * @throws RuntimeException
-     *
-     * @return Repository
+     * Generate the modules folder.
      */
-    public function getModuleRepository()
+    public function generateModulesFolder()
     {
-        $modules = $this->laravel['modules'];
-        if (!$modules instanceof Repository) {
-            throw new RuntimeException("Module repository not found!");
-        }
-        return $modules;
-    }
-
-    /**
-     * @param $name
-     *
-     * @throws RuntimeException
-     *
-     * @return Module
-     */
-    public function getModuleByName($name)
-    {
-        $modules = $this->getModuleRepository();
-        if ($modules->has($name) === false) {
-            throw new RuntimeException("Module [$name] does not exists.");
-        }
-
-        return $modules->get($name);
-    }
-
-    /**
-     * @param Module $module
-     *
-     * @return void
-     */
-    public function moduleSeed(Module $module)
-    {
-        $seeders = [];
-        $name = $module->getName();
-        $config = $module->get('seed');
-        if (is_array($config) && array_key_exists('seeds', $config)) {
-            foreach ((array)$config['seeds'] as $class) {
-                if (@class_exists($class)) {
-                    $seeders[] = $class;
-                }
-            }
-        } else {
-            $class = $this->getSeederName($name); //legacy support
-            if (@class_exists($class)) {
-                $seeders[] = $class;
-            }
-        }
-
-        if (count($seeders) > 0) {
-            array_walk($seeders, [$this, 'dbSeed']);
-            $this->info("Module [$name] seeded.");
-        }
-    }
-
-    /**
-     * Seed the specified module.
-     *
-     * @param string $className
-     *
-     * @return array
-     */
-    protected function dbSeed($className)
-    {
-        $params = [
-            '--class' => $className,
-        ];
-
-        if ($option = $this->option('database')) {
-            $params['--database'] = $option;
-        }
-
-        if ($option = $this->option('force')) {
-            $params['--force'] = $option;
-        }
-
-        $this->call('db:seed', $params);
-    }
-
-    /**
-     * Get master database seeder name for the specified module.
-     *
-     * @param string $name
-     *
-     * @return string
-     */
-    public function getSeederName($name)
-    {
-        $name = Str::studly($name);
-
-        $namespace = $this->laravel['modules']->config('namespace');
-        $namespace .= '\\' . $name;
-        $namespace .= '\\' . trim(str_replace('/', '\\', $this->getSeedPath()), '\\');
-        $namespace .= '\\' . $name . 'DatabaseSeeder';
-
-        return $namespace;
-    }
-    
-    /**
-     * @return string
-     */
-    private function getSeedPath()
-    {
-    	return $this->laravel['modules']->config('paths.generator.seed');
-    }
-
-    /**
-     * Get the console command arguments.
-     *
-     * @return array
-     */
-    protected function getArguments()
-    {
-        return array(
-            array('module', InputArgument::OPTIONAL, 'The name of module will be used.'),
+        $this->generateDirectory($this->laravel['modules']->config('paths.modules'),
+            'Modules directory created successfully',
+            'Modules directory already exist'
         );
     }
 
     /**
-     * Get the console command options.
-     *
-     * @return array
+     * Generate the assets folder.
      */
-    protected function getOptions()
+    public function generateAssetsFolder()
     {
-        return array(
-            array('database', null, InputOption::VALUE_OPTIONAL, 'The database connection to seed.'),
-            array('force', null, InputOption::VALUE_NONE, 'Force the operation to run when in production.'),
+        $this->generateDirectory($this->laravel['modules']->config('paths.assets'),
+            'Assets directory created successfully',
+            'Assets directory already exist'
         );
+    }
+
+    /**
+     * Generate the specified directory by given $dir.
+     *
+     * @param $dir
+     * @param $success
+     * @param $error
+     */
+    protected function generateDirectory($dir, $success, $error)
+    {
+        if (!$this->laravel['files']->isDirectory($dir)) {
+            $this->laravel['files']->makeDirectory($dir);
+
+            $this->info($success);
+
+            return;
+        }
+
+        $this->error($error);
     }
 }
