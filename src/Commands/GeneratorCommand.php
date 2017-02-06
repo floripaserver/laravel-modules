af0323a316d4f305ff138fc58cef0ffbf16324ca
xuanhoa88 <xuan.0211@gmail.com>
2017-02-06 10:22:05 +0700

    Update GeneratorCommand.php

--- src/Commands/GeneratorCommand.php
+++ src/Commands/GeneratorCommand.php
@@ -63,7 +63,7 @@ abstract class GeneratorCommand extends Command
     /**
      * Get class namespace.
      *
-     * @param \Nwidart\Modules\Module $module
+     * @param \Llama\Modules\Module $module
      *
      * @return string
      */
